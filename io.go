badc1d1cbbdc23ed90adbcbecc897abc8cff74e9
sinmetal <metal.tie@gmail.com>
2020-01-05 22:05:31 +0900

    Typeを適当に合成

--- io.go
+++ io.go
@@ -7,16 +7,6 @@ import (
 	"github.com/disintegration/imaging"
 )
 
-func Write(w io.Writer, img image.Image, format FormatType) error {
-	var f imaging.Format
-	switch format {
-	case PNG:
-		f = imaging.PNG
-	case JPEG:
-		f = imaging.JPEG
-	default:
-		f = imaging.PNG
-	}
-
-	return imaging.Encode(w, img, f)
+func Write(w io.Writer, img image.Image, ft FormatType) error {
+	return imaging.Encode(w, img, ft.ImagingFormat())
 }
--- resize_test.go
+++ resize_test.go
@@ -6,6 +6,7 @@ import (
 
 	"cloud.google.com/go/storage"
 	"github.com/disintegration/imaging"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestResize(t *testing.T) {
@@ -17,33 +18,34 @@ func TestResize(t *testing.T) {
 		name                   string
 		srcObject              string
 		dstObject              string
-		wantContentType        string
+		wantGomaType           GomaType
 		dstWidth               int
 		dstHeight              int
 		dstContentType         imaging.Format
 		withStorageWriteOption []WithStorageWriteOption
 	}{
-		{"png", "shingo_nouhau.png", "shingo_nouhau_600.png", "image/png", 600, 0, imaging.PNG, []WithStorageWriteOption{WithMaxAge(600)}},
-		{"png2", "sinmetal-merpay.png", "sinmetal-merpay_600.png", "image/png", 600, 0, imaging.PNG, []WithStorageWriteOption{WithMaxAge(600)}},
-		{"jpeg", "shingo.jpg", "shingo_600.jpg", "image/jpeg", 600, 0, imaging.JPEG, []WithStorageWriteOption{WithMaxAge(600)}},
-		{"no-change-size", "sinmetal.jpg", "sinmetal_640.jpg", "image/jpeg", 640, 0, imaging.JPEG, []WithStorageWriteOption{WithMaxAge(600)}},
-		{"mini-to-mini", "sinmetal.jpg", "sinmetal_320.jpg", "image/jpeg", 320, 0, imaging.JPEG, []WithStorageWriteOption{WithMaxAge(600)}},
-		{"no-cache", "sinmetal.jpg", "sinmetal_no_cache.jpg", "image/jpeg", 320, 0, imaging.JPEG, []WithStorageWriteOption{}},
+		{"png", "shingo_nouhau.png", "shingo_nouhau_600.png", GomaType{PNG, "image/png"}, 600, 0, imaging.PNG, []WithStorageWriteOption{WithMaxAge(600)}},
+		{"png2", "sinmetal-merpay.png", "sinmetal-merpay_600.png", GomaType{PNG, "image/png"}, 600, 0, imaging.PNG, []WithStorageWriteOption{WithMaxAge(600)}},
+		{"jpeg", "shingo.jpg", "shingo_600.jpg", GomaType{JPEG, "image/jpeg"}, 600, 0, imaging.JPEG, []WithStorageWriteOption{WithMaxAge(600)}},
+		{"no-change-size", "sinmetal.jpg", "sinmetal_640.jpg", GomaType{JPEG, "image/jpeg"}, 640, 0, imaging.JPEG, []WithStorageWriteOption{WithMaxAge(600)}},
+		{"mini-to-mini", "sinmetal.jpg", "sinmetal_320.jpg", GomaType{JPEG, "image/jpeg"}, 320, 0, imaging.JPEG, []WithStorageWriteOption{WithMaxAge(600)}},
+		{"no-cache", "sinmetal.jpg", "sinmetal_no_cache.jpg", GomaType{JPEG, "image/jpeg"}, 320, 0, imaging.JPEG, []WithStorageWriteOption{}},
 	}
 
 	for _, tt := range cases {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			src, ct, err := s.Read(ctx, "sinmetal", tt.srcObject)
+			src, gt, err := s.Read(ctx, "sinmetal", tt.srcObject)
 			if err != nil {
 				t.Fatal(err)
 			}
-			if e, g := tt.wantContentType, ct; e != g {
-				t.Errorf("Content Type want %v got %v", e, g)
+
+			if e, g := tt.wantGomaType, gt; cmp.Equal(tt.wantGomaType, gt) {
+				t.Errorf("GomaType Type want %v got %v", e, g)
 			}
 
 			dst := Resize(src, tt.dstWidth, tt.dstHeight)
-			if err := s.Write(ctx, dst, tt.dstContentType, "sinmetal", tt.dstObject, tt.withStorageWriteOption...); err != nil {
+			if err := s.Write(ctx, dst, gt.FormatType, "sinmetal", tt.dstObject, tt.withStorageWriteOption...); err != nil {
 				t.Fatal(err)
 			}
 			if err := s.AddObjectACL(ctx, "sinmetal", tt.dstObject, storage.AllUsers, storage.RoleReader); err != nil {
@@ -59,30 +61,30 @@ func TestResizeToFitLongSide(t *testing.T) {
 	ctx := context.Background()
 
 	cases := []struct {
-		name            string
-		srcObject       string
-		dstObject       string
-		wantContentType string
-		dstSize         int
-		dstContentType  imaging.Format
+		name           string
+		srcObject      string
+		dstObject      string
+		wantGomaType   GomaType
+		dstSize        int
+		dstContentType imaging.Format
 	}{
-		{"png", "shingo_nouhau.png", "shingo_nouhau_300.png", "image/png", 300, imaging.PNG},
-		{"jpeg", "shingo.jpg", "shingo_300.jpg", "image/jpeg", 300, imaging.JPEG},
+		{"png", "shingo_nouhau.png", "shingo_nouhau_300.png", GomaType{PNG, "image/png"}, 300, imaging.PNG},
+		{"jpeg", "shingo.jpg", "shingo_300.jpg", GomaType{JPEG, "image/jpeg"}, 300, imaging.JPEG},
 	}
 
 	for _, tt := range cases {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			src, ct, err := s.Read(ctx, "sinmetal", tt.srcObject)
+			src, gt, err := s.Read(ctx, "sinmetal", tt.srcObject)
 			if err != nil {
 				t.Fatal(err)
 			}
-			if e, g := tt.wantContentType, ct; e != g {
+			if e, g := tt.wantGomaType, gt; cmp.Equal(e, g) {
 				t.Errorf("Content Type want %v got %v", e, g)
 			}
 
 			dst := ResizeToFitLongSide(src, tt.dstSize)
-			if err := s.Write(ctx, dst, tt.dstContentType, "sinmetal", tt.dstObject); err != nil {
+			if err := s.Write(ctx, dst, gt.FormatType, "sinmetal", tt.dstObject); err != nil {
 				t.Fatal(err)
 			}
 		})
